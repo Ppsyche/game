021d113dc52ba734512e47061f3556b402daaee6
Psychee <15145032896@163.com>
2017-07-06 16:08:34 +0800

    整理

--- js/2048.js
+++ js/2048.js
@@ -5,6 +5,28 @@ newGame();
 $("#newGmBtn").on("click",function(){
 	newGame();
 })
+ function randomIntNum(min,max,num){//获取num个min~max之间的数字,min与max正负均可
+	if(isNaN(min))
+		min = 0;
+	if(isNaN(max))
+		max = 1;
+	if(isNaN(num))
+		num = 1;
+	var result = [];
+	var t = 0;
+	while(t != num) {
+		var p = Math.ceil(Math.random()*(max-min+1))+min-1;
+		for (var i = 0; i < t; i++) {
+			if(p == result[i])
+				break;
+		}
+		if(i >= t){
+			result.push(p);
+			t++;
+		}
+	}
+	return result;
+}
 function newGame(){
 	$("#gameover").hide();
 	for (var i = 0; i < 4; i++) {
--- js/sudoku.js
+++ js/sudoku.js
@@ -1,3 +1,25 @@
+ function randomIntNum(min,max,num){//获取num个min~max之间的数字,min与max正负均可
+	if(isNaN(min))
+		min = 0;
+	if(isNaN(max))
+		max = 1;
+	if(isNaN(num))
+		num = 1;
+	var result = [];
+	var t = 0;
+	while(t != num) {
+		var p = Math.ceil(Math.random()*(max-min+1))+min-1;
+		for (var i = 0; i < t; i++) {
+			if(p == result[i])
+				break;
+		}
+		if(i >= t){
+			result.push(p);
+			t++;
+		}
+	}
+	return result;
+}
  function crosswise(locat,num){//判断横排是否存在相同值
 	var t = Math.floor(locat/9)*9;
 	for (var i = 0; i < 9; i++) {
