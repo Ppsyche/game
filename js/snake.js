3b6d2ce7be08b7f8e002d5023e3b490fd8642ee0
Psychee <15145032896@163.com>
2017-07-06 15:59:26 +0800

    完成

--- js/snake.js
+++ js/snake.js
@@ -1,38 +1,18 @@
-// window.onload = function(){
-// 	var oBtn=document.getElementById("btn");
-// 	window.onkeydown=attachEvents;
-// 	oBtn.onclick=function (e) {
-// 		oBtn.blur();
-// 		if (statenum==0) {
-// 			start();
-// 			statenum=1;
-// 		} else if(statenum==1) {
-// 			clearInterval(snaketimer);
-// 			statenum=2;
-// 		} else {
-// 			snaketimer=setInterval(step,3000/speed);
-// 			statenum=1;
-// 		}
-// 	}
-// }
 var snake = [];
 var l = 0;
 var t = 0;
 var keyPrev = 0;
 var timer = null;
+var score = 0;
+var btn = true;
 fountTable();
 $('.btn').on('click',function(){
-	start();
-	randFood();
-	keyDown();
+	if(btn){
+		start();
+		keyDown();
+		btn = false;
+	}
 });
-
-
-
-
-
-
-
 function fountTable(){
 	$('<table>').appendTo($('.box'));
 	for(var i=0; i<20; i++){
@@ -46,35 +26,63 @@ function fountTable(){
 	}
 }
 function snakeLocation(){
+	$('td').removeClass('snake');
 	for (var i = 0; i < snake.length; i++) {
-		$('tr').eq(snake[i][0]).find('td').eq(snake[i][1]).css({'background':'#ff0'});
+		$('tr').eq(snake[i][0]).find('td').eq(snake[i][1]).addClass('snake');
 	}
-	$('tr').eq(snake[snake.length-1][0]).find('td').eq(snake[snake.length-1][1]).css({'background':'#f00'});
 }
 function start(){
 	for(i=5; i<10; i++){
 		snake.push([4,i]);
 	}
 	snakeLocation();
+	randFood();
+	move(0,1);
+}
+function ifFoodOnSnake(){
+	for(var i=0; i<snake.length-1; i++){
+		if(snake[i][0]==t&&snake[i][1]==l){
+			return true;
+		}
+	}
+	return false;
 }
 function randFood(){
-	l = Math.floor(Math.random()*21);
-	t = Math.floor(Math.random()*21);
-	$('tr').eq(t).find('td').eq(l).css({'background':'#0ff'});
+	$('tr').eq(t).find('td').eq(l).removeClass('food');
+	do{
+		l = Math.floor(Math.random()*20);
+		t = Math.floor(Math.random()*20);
+	}while(ifFoodOnSnake());	
+	$('tr').eq(t).find('td').eq(l).addClass('food');
+}
+function ifOver(){
+	for(var i=0; i<snake.length-1; i++){
+		if((snake[i][0]==snake[snake.length-1][0]&&snake[i][1]==snake[snake.length-1][1])
+			||snake[snake.length-1][0]<0
+			||snake[snake.length-1][0]>=20
+			||snake[snake.length-1][1]<0
+			||snake[snake.length-1][1]>=20){
+			clearInterval(timer);
+			alert('GAME OVER!');
+			document.location.reload();
+			return;
+		}
+	}
 }
 function move(shu,heng){
 	clearInterval(timer);
 	timer = setInterval(function(){
-		var st = snake[snake.length-1][0] + shu;
-		var sl = snake[snake.length-1][1] + heng;
-		for(var i=snake.length-1; i>0; i--){
-			snake[i] = snake[i-1];
+		snake.push([(snake[snake.length-1][0] + shu),(snake[snake.length-1][1] + heng)]);
+		if((snake[snake.length-1][0]==t&&snake[snake.length-1][1]==l)){
+			randFood();
+			score+=2;
+			$('#your-score').text('得分：'+score);
+		}else{
+			snake.shift();
 		}
-		snake.push([st,sl]);
-		var s = snake.shift();
-		$('tr').eq(s[0]).find('td').eq(s[1]).css({'background':'#eee'});
+		ifOver();
 		snakeLocation();
-	},1000);
+	},300);
 }
 function keyDown(){
 	$("body").on("keydown",function(e){
